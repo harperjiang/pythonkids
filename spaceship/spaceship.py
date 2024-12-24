814627415ac0cf3a6cf46381d07d767093b2b9b0
harper <harperjiang@msn.com>
2024-12-24 17:55:25 -0500

    Spaceship Lesson 1: Draw

---
+++ spaceship/spaceship.py
@@ -0,0 +1,39 @@
+import pygame
+
+class Object:
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+class Spaceship(Object):
+    def __init__(self, x, y):
+        super().__init__(x, y, 120, 90)
+
+    def draw(self, window):
+        pygame.draw.rect(window, (255, 200, 124), [self.x, self.y, 40, 20], 0) # top wing
+        pygame.draw.rect(window, (255, 255, 255), [self.x + 20, self.y + 20, 70, 50], 0) # body
+        pygame.draw.rect(window, (255, 200, 124), [self.x, self.y + 70, 40, 20], 0) # bottom wing
+        pygame.draw.rect(window, (255, 223, 0), [self.x + 90, self.y + 40, 30, 10], 0) # laser gun
+
+
+class Bullet(Object):
+    def __init__(self, x, y):
+        super().__init__(x, y, 10, 5)
+
+
+class Monster(Object):
+    def __init__(self, x, y):
+        super().__init__(x, y, 40, 40)
+
+    def draw(self, window):
+        pygame.draw.rect(window, (255, 0, 0), [self.x, self.y, 40, 40], 0)
+        pygame.draw.rect(window, (0, 0, 0), [self.x + 4, self.y + 8, 12, 8], 0) # left eye
+        pygame.draw.rect(window, (0, 0, 0), [self.x + 24, self.y + 8, 12, 8], 0) # right eye
+        pygame.draw.rect(window, (0, 0, 0), [self.x + 4, self.y + 24, 32, 8], 0) # mouth
+
+
+class RewardItem(Object):
+    def __init__(self, x, y):
+        super().__init__(x, y, 40, 40)
---
+++ spaceship/spaceship_main.py
@@ -0,0 +1,23 @@
+import pygame
+from pandocfilters import Space
+
+from spaceship import Spaceship, Monster
+
+pygame.init()
+
+window = pygame.display.set_mode((1200, 500))
+
+running = True
+
+spaceship = Spaceship(100,100)
+
+monster001 = Monster(500, 100)
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    spaceship.draw(window)
+    monster001.draw(window)
+
+    pygame.display.update()
