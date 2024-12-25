d25dd1e1e18d816159cc27e1b93cb22faaf13cde
harper <harperjiang@msn.com>
2024-12-25 16:08:34 -0500

    Spaceship Lesson 2: Move

--- spaceship/spaceship_main.py
+++ spaceship/spaceship_main.py
@@ -1,6 +1,6 @@
 import pygame
 
-from spaceship import Spaceship, Monster
+from spaceship import Spaceship, Monster, RewardItem
 
 pygame.init()
 
@@ -11,14 +11,22 @@ running = True
 spaceship = Spaceship(100,100)
 
 monster001 = Monster(500, 100)
+reward001 = RewardItem(600, 200)
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_DOWN:
+                spaceship.move((0, 50))
+            if event.key == pygame.K_UP:
+                spaceship.move((0, -50))
+
     window.fill((0, 0, 0))
 
     spaceship.draw(window)
     monster001.draw(window)
+    reward001.draw(window)
 
     pygame.display.update()
